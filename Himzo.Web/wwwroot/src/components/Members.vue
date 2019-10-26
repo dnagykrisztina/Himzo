7cf9d63eaa5e35ddd1a45826ba7b3791f7eb7849
Dörner Virag <mal.dorner.virag@gmail.com>
2019-10-27 00:19:34 +0200

    vadi uj vue-s projekt (legyszi engedd magad felpusholni)

---
+++ Himzo.Web/wwwroot/src/components/Members.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header></Header>
+    <Title></Title>
+    <MembersBody></MembersBody>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Title from "./Title.vue";
+import MembersBody from "./MembersBody.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "MembersBody",
+  props: {},
+  components: {
+    Header,
+    Title,
+    MembersBody,
+    Footer
+  }
+};
+</script>
---
+++ Himzo.Web/wwwroot/src/components/_userorder.vue
@@ -0,0 +1,27 @@
+<template>
+     <div class="order">
+        <div class="jumbotron" >
+          <div class ="icon">
+              <i v-bind:class="order.statusIcon"></i>
+          </div>
+          <div class="status">
+            <h2>{{ order.amount }} db {{ order.productType }}</h2>
+            <h1>{{ order.statusText }}</h1>
+          </div>
+          <div>
+            <p class="lead">{{ order.message }}</p>
+            <p class="lead">{{ order.time }}</p>
+          </div>
+          <a class="btn btn-lg btn-primary" href="/docs/4.3/components/navbar/" role="button">Rendelés adatai &raquo;</a>
+       </div>
+      
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "userorder",
+  props: ["order"],
+};
+</script>
