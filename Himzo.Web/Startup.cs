4587ec02b98f9761f73608f808edb4e1dd6c1121
Klaudia Demjén <demjenk@sch.bme.hu>
2019-10-27 10:35:37 +0100

    Add swagger API documentation to the project.

--- Himzo.Web/Startup.cs
+++ Himzo.Web/Startup.cs
@@ -14,6 +14,7 @@ using Himzo.Dal;
 using Himzo.Dal.Entities;
 using Himzo.Dal.SeedInterfaces;
 using Himzo.Dal.SeedService;
+using Microsoft.OpenApi.Models;
 
 namespace Himzo.Web
 {
@@ -36,6 +37,11 @@ namespace Himzo.Web
                 AddEntityFrameworkStores<HimzoDbContext>();
             services.AddScoped<IUserSeedService, UserSeedService>();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Himzo API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +51,12 @@ namespace Himzo.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Himzo API V1");
+            });
 
             app.UseRouting();
             app.UseAuthorization();
@@ -56,6 +68,7 @@ namespace Himzo.Web
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
