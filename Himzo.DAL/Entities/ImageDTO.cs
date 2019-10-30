c006d7ac533e6cd31b8711ea22e5db2a83ffd619
Klaudia Demjén <demjenk@sch.bme.hu>
2019-10-30 20:23:49 +0100

    Add DTO classes.

---
+++ Himzo.DAL/Entities/ImageDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Himzo.Dal.Entities
+{
+    public class ImageDTO
+    {
+        public int ImageId { get; set; }
+        public byte[] ByteImage { get; set; }
+    }
+}
