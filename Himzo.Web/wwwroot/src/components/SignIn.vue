0336e0feed7a556c34b8204210e4d0cf978b3e5a
Krisztina Dalma Nagy <dnagykrisztina@sch.bme.hu>
2019-11-01 17:28:40 +0100

    Add facebook authentication, wire it to frontend and add fdefault data to database

--- Himzo.Web/wwwroot/src/components/SignIn.vue
+++ Himzo.Web/wwwroot/src/components/SignIn.vue
@@ -1,30 +1,38 @@
 <template>
 <div>
-  <header>
-    <link href="signin_reg.css" rel="stylesheet" />
-  </header>
+    <head>
+        <link href="signin_reg.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/cd3ec43ff2.js" crossorigin="anonymous"></script>
+    </head>
   <body class="text-center">
-    <form class="form-signin">
-      <img class="rounded-circle" src="./pic/logo.png" alt width="100" height="100" />
-      <h1 class="h3 mb-3 font-weight-normal">Bejelentkezés</h1>
-      <label for="inputEmail" class="sr-only">E-mail cím</label>
-      <input
-        type="email"
-        id="inputEmail"
-        class="form-control"
-        placeholder="E-mail cím"
-        required
-        autofocus
-      />
-      <label for="inputPassword" class="sr-only">Jelszó</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" required />
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Bejelentkezés</button>
-      <label>
-        Nincs fiókod?
-        <a href="index#/registration">Regisztrálj</a>
-      </label>
-      <p class="mt-5 mb-3 text-muted">&copy; Pulcsi és FoltMéKör</p>
-    </form>
+      <form class="form-signin">
+          <img class="rounded-circle" src="./pic/logo.png" alt width="100" height="100" />
+          <h1 class="h3 mb-3 font-weight-normal">Bejelentkezés</h1>
+          <label for="Email" class="sr-only">E-mail cím</label>
+          <input type="email"
+                 id="Email"
+                 class="form-control"
+                 placeholder="E-mail cím"
+                 required
+                 autofocus />
+          <label for="Password" class="sr-only">Jelszó</label>
+          <input type="password" id="Password" class="form-control" placeholder="Jelszó" required />
+          <br />Maradj bejelentkezve
+          <input type="checkbox" id="KeepMeSignIn" checked />
+          <button class="btn btn-lg btn-primary btn-block" type="button" onclick="login()">Bejelentkezés</button>
+          <a class="btn btn-block btn-social btn-google" style="color: white" onclick="googleLogin()">
+              <span class="fab fa-google"></span> Google
+          </a>
+          <a class="btn btn-block btn-social btn-facebook" style="color: white" onclick="facebookLogin()">
+              <span class="fab fa-facebook"></span> Facebook
+          </a>
+          <label>
+              Nincs fiókod?
+              <a href="index#/registration">Regisztrálj</a>
+          </label>
+          <p class="mt-5 mb-3 text-muted">&copy; Pulcsi és FoltMéKör</p>
+      </form>
   </body>
 </div>
 </template>
