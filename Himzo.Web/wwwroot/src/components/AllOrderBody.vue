1724d509da2000b8561f9520b1483dc5a100af46
Sinkó Sára <sara@sinko.hu>
2019-11-10 16:20:25 +0100

    call the api, images, most épp nem a legszebb állapotában van

--- Himzo.Web/wwwroot/src/components/AllOrderBody.vue
+++ Himzo.Web/wwwroot/src/components/AllOrderBody.vue
@@ -1,79 +1,39 @@
 <template>
-    <body>
-        <main role="main" class="container">
+<body>
+  <main role="main" class="container">
+    <order v-for="order in orders" v-bind:key="order.id" v-bind:order="order"></order>
 
-            <order
-                v-for="order in orders"
-                v-bind:key="order.id"
-                v-bind:order="order"
-            ></order>
-
-            <hr class="featurette-divider" />
-
-        </main>
-    </body>
-
+    <hr class="featurette-divider" />
+  </main>
+</body>
 </template>
 
  
  <script>
-    import order from "./_order.vue";
+import order from "./_order.vue";
+import axios from "axios";
 
-    export default {
-        name: "AllOrderBody",
-        props: {},
-        components: {
-            order
-        },
-        data() {
-            return {
-            orders: [
-                {
-            id: 1,
-            name: "Sinkó Sára",
-            statusIcon: "fas fa-check-circle  fa-3x",
-            amount: "51",
-            productType: "folt",
-            statusText: "Kész",
-            message: "Jöhetsz érte.",
-            time: "2025-03-03"
-            },
-            {
-            id: 2,
-            name: "Egy Másik Ember",
-            statusIcon: "fas fa-clock  fa-3x",
-            amount: "5",
-            productType: "minta",
-            statusText: "Folyamatban",
-            message: "A mintád fele van kész",
-            time: "2018-03-03"
-            },
-            {
-            id: 3,
-            name: "Gutai Auguszta Matild",
-            statusIcon: "fas fa-times-circle  fa-3x",
-            amount: "10",
-            productType: "folt",
-            statusText: "Rendelés elutasítva",
-            message: "Jelenleg nem fogadunk rendeléseket.",
-            time: "2019-10-13"
-            },
-            {
-            id: 4,
-            name: "Random Megrendelő",
-            statusIcon: "fas fa-cog  fa-3x",
-            amount: "450",
-            productType: "folt",
-            statusText: "Rendelés feldolgozás alatt",
-            message: "",
-            time: ""
-            }
-        ]
+export default {
+  name: "AllOrderBody",
+  props: {},
+  components: {
+    order
+  },
+  data() {
+    return {
+      orders: []
     };
+  },
+  async mounted() {
+    try {
+      const res = await axios.get(
+        "http://localhost:52140/api/Orders/?all=true"
+      );
+      this.orders = res.data;
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
-
 </script>
-
-<style scoped>
-</style>
+	
--- Himzo.Web/wwwroot/src/components/_userorder.vue
+++ Himzo.Web/wwwroot/src/components/_userorder.vue
@@ -1,27 +1,38 @@
 <template>
-     <div class="order">
-        <div class="jumbotron" >
-          <div class ="icon">
-              <i v-bind:class="order.statusIcon"></i>
-          </div>
-          <div class="status">
-            <h2>{{ order.amount }} db {{ order.productType }}</h2>
-            <h1>{{ order.statusText }}</h1>
-          </div>
-          <div>
-            <p class="lead">{{ order.message }}</p>
-            <p class="lead">{{ order.time }}</p>
-          </div>
-          <a class="btn btn-lg btn-primary" href="/docs/4.3/components/navbar/" role="button">Rendelés adatai &raquo;</a>
-       </div>
-      
+  <div class="order">
+    <div class="jumbotron">
+      <div class="fa">
+        <i v-bind:class="order.statusIcon"></i>
+        <i v-if="order.orderState === 0" value="fas fa-cog">&#xf013;</i>
+        <i v-else-if="order.orderState === 1" value="fas fa-clock ">&#xf017;</i>
+        <i v-else-if="order.orderState === 2" value="fas fa-check-circle ">&#xf058;</i>
+        <i v-else-if="order.orderState === 3" value="fas fa-times-circle ">&#xf057;</i>
+      </div>
+      <div class="status">
+        <h1>{{ order.orderId }}</h1>
+        <h2 v-if="order.type === 1">{{ order.amount }} db hímzés</h2>
+        <h2 v-else-if="order.type === 0">{{ order.amount }} db folt</h2>
+        <h2 v-if="order.orderstate === 0">Rendelés feldolgozás alatt</h2>
+        <h2 v-else-if="order.orderstate === 1">Folyamatban</h2>
+        <h2 v-else-if="order.orderstate === 2">Kész</h2>
+        <h2 v-if="order.orderstate === 3">Rendelés elutasítva</h2>
+      </div>
+      <div>
+        <p class="lead">{{ order.commentContent }}</p>
+        <p class="lead">{{ order.commentUpdateTime }}</p>
+      </div>
+      <a
+        class="btn btn-lg btn-primary"
+        href="/docs/4.3/components/navbar/"
+        role="button"
+      >Rendelés adatai &raquo;</a>
     </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "userorder",
-  props: ["order"],
+  props: ["order"]
 };
 </script>
