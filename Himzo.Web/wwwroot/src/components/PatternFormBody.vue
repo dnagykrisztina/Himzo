b6ab39803a56878dfe31d95b4b941fa7f4c0b6c2
Sinkó Sára <sara@sinko.hu>
2019-11-23 12:17:01 +0100

    post forms

--- Himzo.Web/wwwroot/src/components/PatternFormBody.vue
+++ Himzo.Web/wwwroot/src/components/PatternFormBody.vue
@@ -1,131 +1,130 @@
 <template>
 <div>
- <div class="jumbotron">
-      <div class="container">
-        <h1 class="display-3">{{ patternFormTitle }}</h1>
-        <p>{{ patternFormDescription }}</p>
-      </div>
-    </div>
-<body>
-  <main role="main">
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-   
-
+  <div class="jumbotron">
     <div class="container">
-      <div class="row">
-        <div class="col-md-8 order-md-1">
-          <form class="needs-validation" validate>
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <input type="file" class="custom-file-input" id="customFile" @change="setImage" />
-                <label class="custom-file-label" for="customFile">{{chooseFile}}</label>
-              </div>
-            </div>
+      <h1 class="display-3">{{ patternFormTitle }}</h1>
+      <p>{{ patternFormDescription }}</p>
+    </div>
+  </div>
+  <body>
+    <main role="main">
+      <!-- Main jumbotron for a primary marketing message or call to action -->
 
-            <div class="row">
-              <div class="col-md-3 mb-3">
-                <label for="size">{{size}}</label>
-                <input
-                  v-model="inputSize"
-                  type="number"
-                  class="form-control"
-                  id="size"
-                  placeholder
-                  required
-                />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 order-md-1">
+            <form class="needs-validation" validate>
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <input type="file" class="custom-file-input" id="customFile" @change="setImage" />
+                  <label class="custom-file-label" for="customFile">{{chooseFile}}</label>
+                </div>
               </div>
-              <div class="col-md-3 mb-3">
-                <label for="amount">{{amount}}</label>
-                <input
-                  v-model="inputAmount"
-                  type="number"
-                  class="form-control"
-                  id="amount"
-                  placeholder
-                  required
-                />
-              </div>
-              <div class="col-md-4 mb-3">
-                <label for="deadline">{{deadline}}</label>
-                <input
-                  type="date"
-                  class="form-control"
-                  id="deadline"
-                  placeholder
-                  required
-                  v-model="inputDeadline"
-                />
-              </div>
-            </div>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="patternLocation">{{patternLocation}}</label>
-                <input
-                  v-model="inputPatternLocation"
-                  type="text"
-                  class="form-control"
-                  id="patternLocation"
-                  placeholder
-                  value
-                  required
-                />
-                <div class="invalid-feedback">Kérlek add meg a minta helyét!</div>
+              <div class="row">
+                <div class="col-md-3 mb-3">
+                  <label for="size">{{size}}</label>
+                  <input
+                    v-model="inputSize"
+                    type="text"
+                    class="form-control"
+                    id="size"
+                    placeholder
+                    required
+                  />
+                </div>
+                <div class="col-md-3 mb-3">
+                  <label for="amount">{{amount}}</label>
+                  <input
+                    v-model="inputAmount"
+                    type="number"
+                    class="form-control"
+                    id="amount"
+                    placeholder
+                    required
+                  />
+                </div>
+                <div class="col-md-4 mb-3">
+                  <label for="deadline">{{deadline}}</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    id="deadline"
+                    placeholder
+                    required
+                    v-model="inputDeadline"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="font">{{fonts}}</label>
-                <input
-                  v-model="inputFonts"
-                  type="text"
-                  class="form-control"
-                  id="font"
-                  placeholder
-                  value
-                  required
-                />
-                <div class="invalid-feedback">Kérlek add meg a mintában használt fontot!</div>
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label for="patternLocation">{{patternLocation}}</label>
+                  <input
+                    v-model="inputPatternLocation"
+                    type="text"
+                    class="form-control"
+                    id="patternLocation"
+                    placeholder
+                    value
+                    required
+                  />
+                  <div class="invalid-feedback">Kérlek add meg a minta helyét!</div>
+                </div>
               </div>
-            </div>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="comment">{{comment}}</label>
-                <input
-                  v-model="inputComment"
-                  type="text"
-                  class="form-control"
-                  id="comment"
-                  placeholder
-                  value
-                />
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label for="font">{{fonts}}</label>
+                  <input
+                    v-model="inputFonts"
+                    type="text"
+                    class="form-control"
+                    id="font"
+                    placeholder
+                    value
+                    required
+                  />
+                  <div class="invalid-feedback">Kérlek add meg a mintában használt fontot!</div>
+                </div>
               </div>
-            </div>
 
-            <div class="row">
-              <hr class="mb-4" />
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label for="comment">{{comment}}</label>
+                  <input
+                    v-model="inputComment"
+                    type="text"
+                    class="form-control"
+                    id="comment"
+                    placeholder
+                    value
+                  />
+                </div>
+              </div>
 
-              <a
-                class="btn btn-primary"
-                @click="reset"
-                type="reset"
-                href="index.html"
-              >{{cancelButton}}</a>
-              <a
-                class="btn btn-primary"
-                type="submit"
-                href="index.html#/userorder"
-                @click="postPost()"
-              >{{orderButton}}</a>
-            </div>
-          </form>
+              <div class="row">
+                <hr class="mb-4" />
+
+                <a
+                  class="btn btn-primary"
+                  @click="reset"
+                  type="reset"
+                  href="index.html"
+                >{{cancelButton}}</a>
+                <a
+                  class="btn btn-primary"
+                  type="submit"
+                  href="index.html#/userorder"
+                  @click="postPost()"
+                >{{orderButton}}</a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-</body>
+    </main>
+  </body>
 </div>
 </template>
 
@@ -184,6 +183,7 @@ export default {
     },
     setImage: function(e) {
       const file = e.target.files[0];
+      this.chooseFile = e.target.files[0].name;
       console.log(file);
 
       if (!file.type.includes("image/")) {
